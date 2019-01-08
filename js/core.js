524e201b442e079bd032ccc4687c028150156930
Bartek <bartek@appselio.pl>
2019-01-08 18:29:16 +0100

    Added api client for searching products.

--- js/core.js
+++ js/core.js
@@ -15,7 +15,36 @@ $(document).ready(function() {
 	});
 
 	function searchProducts(){
+        
+        let query = $("#query").val();
 
+		fetch(`${api}/search/?query=${query}`)
+		.then(resp => resp.json())
+		.then(resp => {
+
+			$("#result").html("");
+
+			if(resp.length == 0){
+
+				$("#result").html('<p class="alert alert-warning">There is no results.</strong></p>');
+
+			} else {
+
+				resp.forEach(product => {
+
+					$("#result").append(`<div class="item col-md-12">
+											<p><strong>${product.name}</strong></p>
+											<p><em>${product.description}</em></p>
+										</div>`);
+				});
+
+			}
+
+		})
+		.catch(error => {
+			$("#result").html('<p class="alert alert-danger">Something gone wrong...</p>');
+			console.error("Err: ", error);
+		});
 	}
 
 });
